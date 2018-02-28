9c1d904b43325bbb0e87eec4e72aa595ef86ef84
Harmen Wierenga <h.wierenga@amolf.nl>
2018-02-28 17:01:24 +0100

    Defined HopDirection enum class in PossibleHop, have to implement this into CrosslinkerContainer

--- PossibleHop.hpp
+++ PossibleHop.hpp
@@ -4,10 +4,17 @@
 #include "Crosslinker.hpp"
 #include "MicrotubuleType.hpp"
 
+enum class HopDirection
+{
+    FORWARD,
+    BACKWARD
+};
+
 struct PossiblePartialHop
 {
     Crosslinker* p_partialLinker;
     SiteLocation locationToHopTo;
+    HopDirection direction;
 };
 
 struct PossibleFullHop
@@ -15,6 +22,7 @@ struct PossibleFullHop
     Crosslinker* p_fullLinker;
     Crosslinker::Terminus terminusToHop;
     SiteLocation locationToHopTo;
+    HopDirection direction;
     // Keep track of the extension, since this can be updated easily
     double oldExtension; // positionOnMobile - positionOnFixed
     double newExtension; // positionOnMobile - positionOnFixed, should differ exactly one lattice spacing from oldExtension
