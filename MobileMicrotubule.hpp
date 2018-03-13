394d6bf8a39b93976945ccd58828fd993b649269
Harmen Wierenga <h.wierenga@amolf.nl>
2018-03-13 14:10:22 +0100

    Checked mobileMicrotubule

--- MobileMicrotubule.hpp
+++ MobileMicrotubule.hpp
@@ -3,11 +3,13 @@
 
 #include "Microtubule.hpp"
 
+/* A derivative of Microtubule, this class adds extra functionality related to movement of the mobile microtubule.
+ * This movement can be seen as relative to the fixed microtubule.
+ */
 
 class MobileMicrotubule : public Microtubule
 {
 private:
-
     double m_position;
 public:
     MobileMicrotubule(const double length, const double latticeSpacing, const double initialPosition = 0.);
