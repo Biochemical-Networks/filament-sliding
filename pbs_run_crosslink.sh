4fa8580df506f52dd579c92197765fcbb51a6c37
Harmen Wierenga <h.wierenga@amolf.nl>
2019-06-24 13:53:08 +0200

    Wrote more extensive comments in pbs script

--- pbs_run_crosslink.sh
+++ pbs_run_crosslink.sh
@@ -14,11 +14,12 @@
 ## set email adress
 #PBS -M h.wierenga@amolf.nl
 
-## start job from the directory it was submitted.
+## start job from the directory from which it was submitted.
 ## NO PBS DIRECTIVES AFTER THIS!
 cd $PBS_O_WORKDIR
 
-## Variables should be defined in the environment of qsub and passed on using the -v option: -v VARIABLE="..."
+## Variables should be defined in the environment of qsub in the single environment variable CLA, via the -v option: -v CLA="...".
+## e.g.: qsub pbs_run_crosslink.sh -v CLA="-gv 10 -np 20"
 if [ -z $CLA ]
 then
 echo "Variable CLA (command line arguments) was not defined. Please define, possibly empty."
