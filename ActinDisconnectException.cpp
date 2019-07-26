30d3c343d9a6c12bc3925c40711db6cc3c8542e4
Harmen Wierenga <h.wierenga@amolf.nl>
2019-07-26 10:37:11 +0200

    Minor bug in actin disconnect exception

--- ActinDisconnectException.cpp
+++ ActinDisconnectException.cpp
@@ -2,7 +2,7 @@
 
 ActinDisconnectException::ActinDisconnectException(const double timeToDisconnect, const double positionOfDisconnect)
     :   GeneralException("The actin disconnected.\nOccured at time " + std::to_string(timeToDisconnect)
-                         + ".\nOccured at position " + std::to_string(positionOfDisconnect) + ".")
+                         + ".\nOccured at position " + std::to_string(positionOfDisconnect) + "."
     )
 {
 }
