95c901e0acd76c4b62e24e54e6a57e6ac94f3219
Harmen Wierenga <h.wierenga@amolf.nl>
2019-07-29 13:38:21 +0200

    Made submit jobs script more flexible, change run names on script level, to prevent overlapping runs with the same name on a concurrent cluster

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-NUMBER_RUNS=128
+# A regular expression to check if it is an integer:
+re='^[0-9]+$'
 
-for (( i=0;i<"${NUMBER_RUNS}";i++ )); do
-qsub pbs_run_crosslink.sh -v CLA="";
-sleep 1; # wait to make sure that the submitted program can create an input file before the next one is submitted
+if [ "$#" -ne 3 ]
+then
+>&2 echo "Not the right number of arguments."
+elif [ -z "$1" ]
+then
+>&2 echo "Run name is empty."
+elif [ -z "$2" ] || [[ ! ($2 =~ $re) ]]
+then
+>&2 echo "Number of runs is empty or not a number."
+elif [ -z "$3" ] || [[ ! ($3 =~ $re) ]]
+then
+>&2 echo "Initial run number is empty or not a number."
+fi
+
+RUN_NAME="$1"
+NUMBER_RUNS="$2"
+RUN_NUMBER_INIT="$3"
+
+MAX=$(($NUMBER_RUNS + $RUN_NUMBER_INIT))
+
+for ((i="$RUN_NUMBER_INIT";i<"$MAX";i++)); do
+qsub pbs_run_crosslink.sh -v CLA="-n ${RUN_NAME}.${i}";
 done
