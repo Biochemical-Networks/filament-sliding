2c3ae4c674bf2479d27cd90618acfe0cc70061fa
Harmen Wierenga <h.wierenga@amolf.nl>
2019-07-29 09:32:15 +0200

    Changed submit_jobs slightly

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NUMBER_RUNS=16
+NUMBER_RUNS=128
 
-for ((i=0;i<"$NUMBER_RUNS";i++)); do
+for (( i=0;i<"${NUMBER_RUNS}";i++ )); do
 qsub pbs_run_crosslink.sh -v CLA="";
-sleep 2; # wait to make sure that the submitted program can create an input file before the next one is submitted
+sleep 1; # wait to make sure that the submitted program can create an input file before the next one is submitted
 done
