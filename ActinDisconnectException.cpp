3d9022d74b7dd1ee87f8d55138e8e4214c95ccf2
Harmen Wierenga <h.wierenga@amolf.nl>
2019-09-04 15:04:54 +0200

    Added data storage to ActinDisconnectException for reading out after runs

--- ActinDisconnectException.cpp
+++ ActinDisconnectException.cpp
@@ -10,7 +10,35 @@ ActinDisconnectException::ActinDisconnectException(const double timeToDisconnect
                          + ".\nThe last tracking occured at time " + std::to_string(timeLastTrackingCompletion)
                          + ".\nThe total time spent on the lattice was " + std::to_string(totalTimeBehindTip)
                          + ".\nThe total time spent on the tip was " + std::to_string(totalTimeOnTip)
-                         + "."
-    )
+                         + "."),
+        m_timeToDisconnect(timeToDisconnect),
+        m_positionOfDisconnect(positionOfDisconnect),
+        m_timeLastTrackingCompletion(timeLastTrackingCompletion),
+        m_totalTimeBehindTip(totalTimeBehindTip),
+        m_totalTimeOnTip(totalTimeOnTip)
 {
+}
+
+double ActinDisconnectException::getTimeToDisconnect() const
+{
+    return m_timeToDisconnect;
+}
+double ActinDisconnectException::getPositionOfDisconnect() const
+{
+    return m_positionOfDisconnect;
+}
+
+double ActinDisconnectException::getTimeLastTrackingCompletion() const
+{
+    return m_timeLastTrackingCompletion;
+}
+
+double ActinDisconnectException::getTotalTimeBehindTip() const
+{
+    return m_totalTimeBehindTip;
+}
+
+double ActinDisconnectException::getTotalTimeOnTip() const
+{
+    return m_totalTimeOnTip;
 }
