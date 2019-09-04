cf97c13dab08ea01644fb1a56d07f3fa2d6ef861
Harmen Wierenga <h.wierenga@amolf.nl>
2019-09-04 15:04:54 +0200

    Added data storage to ActinDisconnectException for reading out after runs

--- ActinDisconnectException.hpp
+++ ActinDisconnectException.hpp
@@ -6,12 +6,24 @@
 
 class ActinDisconnectException : public GeneralException
 {
+private:
+    const double m_timeToDisconnect;
+    const double m_positionOfDisconnect;
+    const double m_timeLastTrackingCompletion;
+    const double m_totalTimeBehindTip;
+    const double m_totalTimeOnTip;
 public:
     ActinDisconnectException(const double timeToDisconnect,
                              const double positionOfDisconnect,
                              const double timeLastTrackingCompletion,
                              const double totalTimeBehindTip,
                              const double totalTimeOnTip);
+
+    double getTimeToDisconnect() const;
+    double getPositionOfDisconnect() const;
+    double getTimeLastTrackingCompletion() const;
+    double getTotalTimeBehindTip() const;
+    double getTotalTimeOnTip() const;
 };
 
 #endif // ACTINDISCONNECTEXCEPTION_HPP
