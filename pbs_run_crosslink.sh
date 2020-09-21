a8736b40bf5c41ef84869720e4a30b5f3e815b13
Harmen Wierenga <h.wierenga@amolf.nl>
2020-09-21 18:10:34 +0200

    Run on high cpu

--- pbs_run_crosslink.sh
+++ pbs_run_crosslink.sh
@@ -6,10 +6,10 @@
 #PBS -N CrossLink
 
 ## set the number of nodes and processes per node
-#PBS -l nodes=1:ppn=1:highcore
+#PBS -l nodes=1:ppn=1:highcpu
 
 ## set queue name
-#PBS -q highcore
+#PBS -q highcpu
 
 ## set max wallclock time
 #PBS -l walltime=300:00:00
